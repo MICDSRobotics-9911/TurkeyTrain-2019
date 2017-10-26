51e295aebde6e52fd32e0b0f56e688ab40c8a6cf
Dhanabal <pdhanabal@micds.org>
2017-10-26 18:32:00 -0500

    haha ok so i just downloaded the sdk and used that with the new code to get 3.4 becauise it's not forked (reeee)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.robotplus.hardware.MotorPair;
 import org.firstinspires.ftc.teamcode.robotplus.hardware.TankDrive;
@@ -12,20 +13,24 @@ import org.firstinspires.ftc.teamcode.robotplus.hardware.TankDrive;
  */
 
 @TeleOp(name = "Driver", group = "Driver")
-public class Driver extends LinearOpMode {
+public class Driver extends OpMode {
     private TankDrive drive;
 
     @Override
-    public void runOpMode() {
+    public void init() {
         this.drive = new TankDrive(hardwareMap);
 
-        while(opModeIsActive()) {
-            this.drive.getLeftMotors().setPowers(gamepad1.left_stick_y);
-            this.drive.getRightMotors().setPowers(gamepad1.right_stick_y);
-            if (gamepad1.x) {
-                this.drive.getLeftMotors().setPowers(0.25 * gamepad1.left_stick_y);
-                this.drive.getRightMotors().setPowers(0.25 * gamepad1.right_stick_y);
-            }
+        drive.getRightMotors().getMotor1().setDirection(DcMotorSimple.Direction.REVERSE);
+        drive.getRightMotors().getMotor2().setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop(){
+        this.drive.getLeftMotors().setPowers(gamepad1.left_stick_y);
+        this.drive.getRightMotors().setPowers(gamepad1.right_stick_y);
+        if (gamepad1.x) {
+            this.drive.getLeftMotors().setPowers(0.25 * gamepad1.left_stick_y);
+            this.drive.getRightMotors().setPowers(0.25 * gamepad1.right_stick_y);
         }
     }
 }
